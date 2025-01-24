90a51d02de432cf45b53385c863cca99979559a2
YanPier31 <yanpiernahuin@gmail.com>
2025-01-24 21:32:43 +0000

    Práctica N°1

---
+++ Práctica-N°1/ParImpar.py
@@ -0,0 +1,5 @@
+numero = int(input("Ingrese un número entero para saber si es par o impar: "))
+if numero % 2 != 0:
+    print("El número es impar.")
+else:
+    print("El número es par.")
---
+++ Práctica-N°1/SumaNumeros.py
@@ -0,0 +1,4 @@
+numero = int(input("Ingrese hasta donde quiere que sea la suma: "))
+resultado = int(((numero)*(numero+1))/2)
+
+print(f"La suma hasta el numero {numero} es: {resultado}")
